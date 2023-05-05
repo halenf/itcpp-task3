e48263b0e2dcca3e583404e63bae8912bbc568ed
Halen Finlay <s210172@students.aie.edu.au>
2023-05-05 10:10:48 +0930

    change the itcpp-task3.cpp file to main.cpp. add instructions that appear when the program is opened.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello User!\nWelcome to the government database for people who don't brush their teeth before bed." << std::endl;
+    std::cout << "Please ensure information disclosed to you through this service is kept confidential, or there may be consequences..." << std::endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
